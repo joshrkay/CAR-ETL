3cec6d9ab26df8b9df9fd35dd1fc6a243c881061
Josh <60823931+joshrkay@users.noreply.github.com>
2026-01-07 14:46:09 -0700

    Delete src directory

--- src/api/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-"""API routes package."""
-from .tenants import router as tenants_router
-from .service_accounts import router as service_accounts_router
-
-__all__ = ["tenants_router", "service_accounts_router"]
